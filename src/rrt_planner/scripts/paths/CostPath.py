702aa9ea0069e1c53ffcf2cfac1637ca0cdeea22
RodsCoimbra <rodscoimbra@gmail.com>
2024-10-12 14:37:02 +0100

    Path metrics (distance and cost)

---
+++ src/rrt_planner/scripts/paths/CostPath.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import OccupancyGrid, Path
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class PathLength:
+    def __init__(self):
+        rospy.init_node('path_length')
+        rospy.Subscriber('/move_base/RRTPlannerROS/global_plan', Path, self.path_callback)
+        rospy.Subscriber('/move_base/global_costmap/costmap', OccupancyGrid, self.save_map)
+        self.map = None
+
+    def save_map(self, msg):
+        rospy.loginfo(f"Map received {msg.info.width}x{msg.info.height}")
+        width, height = msg.info.width, msg.info.height
+        self.res = msg.info.resolution
+        self.origin_x = msg.info.origin.position.x
+        self.origin_y = msg.info.origin.position.y
+        self.map = np.array(msg.data).reshape((height, width)).T
+
+    def path_callback(self, msg):
+        poses = msg.poses
+        num_poses = 0
+        cost = 0
+        for pose in poses:
+            x, y = self.WorldToMap(pose.pose.position.x, pose.pose.position.y)
+            if x:  # If the point is inside the map
+                cost += self.map[x, y]
+                num_poses += 1
+
+        mean_cost = cost / num_poses
+        rospy.loginfo(f"Mean cost of the path: {mean_cost}")
+
+    def WorldToMap(self, wx, wy):
+        if self.map is not None:
+            if not (wx < self.origin_x or wy < self.origin_y):
+                mx = int((wx - self.origin_x) / self.res)
+                my = int((wy - self.origin_y) / self.res)
+
+                # map.shape -> (Width, Height)
+                if mx < self.map.shape[0] and my < self.map.shape[1]:
+                    return mx, my
+
+        return None, None
+
+    def run(self):
+        rospy.spin()
+
+
+if __name__ == '__main__':
+    path_length = PathLength()
+    path_length.run()
--- src/rrt_planner/scripts/paths/PathLength.py
+++ src/rrt_planner/scripts/paths/PathLength.py
@@ -8,25 +8,48 @@ class PathLength:
     def __init__(self):
         rospy.init_node('path_length')
         rospy.Subscriber('/move_base/RRTPlannerROS/global_plan', Path, self.path_callback)
-        self.path_length = 0.0
+        rospy.Subscriber('/amcl_path', Path, self.path_amcl_callback)
+        self.path_length_rrt = 0.0
+        self.amcl_poses = None
+        self.discard_distance = 0.0
 
-    def distance(self, point1, point2):
-        return ((point1.x - point2.x)**2 + (point1.y - point2.y)**2)**0.5
+    def distance(self, poses):
+        dist = 0.0
+        for i in range(0, len(poses)-1):
+            point1 = poses[i].pose.position
+            point2 = poses[i+1].pose.position
+            dist += ((point1.x - point2.x)**2 + (point1.y - point2.y)**2)**0.5
+        return dist
 
     def path_callback(self, msg):
         poses = msg.poses
-        dist = 0.0
 
-        for i in range(0, len(poses)-1):
-            dist += self.distance(poses[i].pose.position, poses[i+1].pose.position)
+        # Calculate the path length of the rrt planner
+        dist_rrt = self.distance(poses)
 
-        self.path_length += dist
-        rospy.loginfo(f"New Path Length: {dist:.3f} with {len(poses)} nodes")
-        rospy.loginfo(f"Path Length Until Now: {self.path_length:.3f}")
+        # Calculate the path length using amcl poses
+        if self.amcl_poses:
+            self.dist_amcl = self.distance(self.amcl_poses)
+
+        # Our rrt didn't send its initial path, so we discard the first path of the amcl to compare
+        if self.path_length_rrt == 0.0:
+            self.discard_distance = self.dist_amcl
+
+        rospy.loginfo(f"Path Length using amcl poses: {(self.dist_amcl-self.discard_distance):.3f}")
+        rospy.loginfo(f"Path Length Until Now rrt planner: {self.path_length_rrt:.3f}")
+
+        # Only updated after printing to compare the two paths lengths
+        self.path_length_rrt += dist_rrt
+
+        rospy.loginfo(f"New Path Length: {dist_rrt:.3f} with {len(poses)} nodes\n")
+
+    def path_amcl_callback(self, msg):
+        self.amcl_poses = msg.poses
 
     def run(self):
         rospy.spin()
-        rospy.loginfo("Total Path length: " + str(self.path_length))
+        rospy.loginfo(f"Total Path length rrt planner: {self.path_length_rrt:.3f}")
+        rospy.loginfo(f"Total path using amcl poses: {(self.dist_amcl-self.discard_distance):.3f}")
 
 
 if __name__ == '__main__':
